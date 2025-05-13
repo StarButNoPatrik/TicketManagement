2d73f247de702cd3433e4cb0c822f0a09ff5982a
Ankika Dey <98796194+ankikadey@users.noreply.github.com>
2025-05-13 15:21:02 +0530

    update Attendee

--- src/Entity/Attendee.java
+++ src/Entity/Attendee.java
@@ -3,8 +3,6 @@ package Entity;
 public class Attendee extends User {
     public Attendee(String name, String id) {
         super(name, id); // Explicitly call the parent class constructor
-        this.name = name;
-        this.id = id;
     }
 @Override
 public void showProfile() {
