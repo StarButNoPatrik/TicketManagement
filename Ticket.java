44bb59c70379b3de861cd744b2743dbfd073ed30
Ankan777 <97438793+Ankan777@users.noreply.github.com>
2025-05-13 14:51:05 +0530

    Add files via upload

---
+++ Ticket.java
@@ -0,0 +1,23 @@
+package eventbooking;
+
+public class Ticket {
+    private Attendee attendee;
+	    private Event event;
+	    private String status;
+	  Ticket(Attendee attendee, Event event){
+		  this.attendee=attendee;
+		  this.event=event;
+		  this.status="Booked";
+	}
+	  public void cancel() {
+		  this.status="Cancelled";
+	  }
+	  public String getStatus() {
+		  return status;
+	  }
+	  public String ticketDetails() {
+		  return "Ticket: " + attendee.name + " - " + event.getTitle() + " - " + status;
+	  }
+	  
+
+}
